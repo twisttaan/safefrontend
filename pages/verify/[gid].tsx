afcee2864a88161f4fcf0085f5a52b45a9aeb171
Tristan Camejo <twisttaan@icloud.com>
2021-11-16 17:06:50 +1100

    fix: purple loading -> blurple

--- pages/verify/[gid].tsx
+++ pages/verify/[gid].tsx
@@ -73,7 +73,7 @@ const Verify: NextPage = () => {
   while (loading) {
     return (
       <div className="flex justify-center items-center">
-        <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-purple-500" />
+        <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-blurple" />
       </div>
     );
   }
