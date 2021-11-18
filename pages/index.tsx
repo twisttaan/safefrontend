58f64b2894a6252899348a732fdfd023953e7f84
Tristan Camejo <twisttaan@icloud.com>
2021-11-18 16:19:14 +1100

    feat: only ping effect when hover

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,7 +29,7 @@ const Home: NextPage = () => {
                   type="button"
                   className="inline-flex items-center justify-center w-full px-8 py-4 text-base font-bold leading-6 text-white bg-indigo-600 border border-transparent rounded-full md:w-auto hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600"
                 >
-                  <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-purple-400 opacity-5"></span>
+                  <span className="absolute inline-flex h-full w-full rounded-full bg-purple-400 opacity-5 hover:animate-ping"></span>
                   Invite Now!
                 </a>
                 <span className="absolute top-0 right-0 px-2 py-1 -mt-3 -mr-6 text-xs font-medium leading-tight text-white bg-green-400 rounded-full">
