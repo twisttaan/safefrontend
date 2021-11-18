2e90ebccb0b672249d6626196282fc918d25f5d0
Tristan <69066026+twisttaan@users.noreply.github.com>
2021-11-18 13:22:53 +1100

    fix: hopefully this works (we need a better debug system)

--- pages/verify/[gid].tsx
+++ pages/verify/[gid].tsx
@@ -120,36 +120,32 @@ const Verify: NextPage = () => {
 
   while (success === true) {
     return (
-      <div className="text-center">
-        verified!
-      </div>
-      <div className="flex justify-center">
-        
-        <br />
-        <svg
-          className="checkmark"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 52 52"
-        >
-          <circle
-            className="checkmark__circle"
-            cx="26"
-            cy="26"
-            r="25"
-            fill="none"
-          />
-          <path
-            className="checkmark__check"
-            fill="none"
-            d="M14.1 27.2l7.1 7.2 16.7-16.8"
-          />
-        </svg>
-        <br />
-        <div className="text-center">
-        you may close this tab now.
-      </div>
-        
-      </div>
+      <>
+        <div className="text-center">verified!</div>
+        <div className="flex justify-center">
+          <br />
+          <svg
+            className="checkmark"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 52 52"
+          >
+            <circle
+              className="checkmark__circle"
+              cx="26"
+              cy="26"
+              r="25"
+              fill="none"
+            />
+            <path
+              className="checkmark__check"
+              fill="none"
+              d="M14.1 27.2l7.1 7.2 16.7-16.8"
+            />
+          </svg>
+          <br />
+        </div>
+        <div className="text-center">you may close this tab now.</div>
+      </>
     );
   }
 
